bd2c112926eb15828cbb7ff74d648d65856ac3d4
IndyraAyuWijayanti <56098058+IndyraAyuWijayanti@users.noreply.github.com>
2022-01-02 19:11:36 +0700

    update

--- p/application/config/config.php
+++ p/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/Pemasaran_Puslitkoka';
-$config['base_url'] = 'http://localhost/p/';
+$config['base_url'] = 'http://localhost/project/p/';
 
 /*
 |--------------------------------------------------------------------------
